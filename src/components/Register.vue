e54a5deddb5b6327e5e5925a338fd52f359f983a
Bauer <daniel.bauer@bechtle.com>
2020-03-27 08:11:40 +0100

    Register Model working Jabber Call

--- src/components/Register.vue
+++ src/components/Register.vue
@@ -39,36 +39,68 @@
   
 <!-- 2. Modal -->
   <b-modal id="modal-3" title="Registrieren" ref="modal3" :hide-footer="true">
-    <form>
-  <div class="form-group">
-    <label for="reason">Grund des Besuchs</label>
-    <input type="text" class="form-control" id="reason" placeholder="Grund des Besuchs">
+        <form>
+        <div class="form-group">
+          <label for="reason">Grund des Besuchs</label>
+          <input type="text" class="form-control" id="reason" placeholder="Grund des Besuchs">
 
-       
-  </div>
+          
+        </div>
 
-<div>
-  <label for="users1">Ansprechpartner</label>
-  <vue-bootstrap-typeahead
-    class="mb-4"
-    v-model="query"
-    :data="users"
-    :serializer="item => item.fullname"
-    @hit="selectedUser = $event"
-    placeholder="Ansprechpartner suchen"
-    id="users1"
-  />
+        <div>
+          <label for="users1">Ansprechpartner</label>
+            <vue-bootstrap-typeahead
+              class="mb-4"
+              v-model="query"
+              :data="users"
+              :serializer="item => item.fullname"
+              @hit="selectedUser = $event"
+              placeholder="Ansprechpartner suchen"
+              id="users1"
+            />
 
- <h3>Selected User JSON</h3>
- <pre>{{ selectedUser | stringify }}</pre>
-</div>
+          <h3>Selected User JSON</h3>
+          <pre>{{ selectedUser | stringify }}</pre>
+        </div>
 
 
 
-  <button class="btn btn-outline-secondary"  @click.prevent="back1()">zurück</button>
-  <button type="submit" class="btn btn-outline-info"  @click.prevent="submitted2()">Weiter</button>
-    </form>
+        <button class="btn btn-outline-secondary"  @click.prevent="back1()">zurück</button>
+        <button type="submit" class="btn btn-outline-info"  @click.prevent="submitted2(selectedUser)">Weiter</button>
+        </form>
   </b-modal>
+
+
+  <b-modal id="modal-4" title="Registrieren" ref="modal4" :hide-footer="true">
+   
+    <div class="form-group">
+    <label for="contact">Sicherheitsunterweisung</label>
+    <p id="contact">
+Der Arbeitgeber hat nach § 12 Arbeitsschutzgesetz die Beschäftigten über Sicherheit und Gesundheitsschutz bei der Arbeit während ihrer Arbeitszeit ausreichend und angemessen zu unterweisen. 
+Die Unterweisung umfasst Anweisungen und Erläuterungen, die eigens auf den Arbeitsplatz oder den Aufgabenbereich der Beschäftigten ausgerichtet sind. 
+Die Unterweisung muss bei der Einstellung, bei Veränderungen im Aufgabenbereich, der Einführung neuer Arbeitsmittel oder einer neuen Technologie vor Aufnahme der Tätigkeit der Beschäftigten erfolgen. 
+Die Unterweisung muss an die Gefährdungsentwicklung angepasst sein und erforderlichenfalls regelmäßig (mindestens einmal im Jahr) wiederholt werden.
+    </p>
+    
+
+    <b-form-checkbox
+      id="checkbox-1"
+      v-model="status"
+      name="checkbox-1"
+      value="accepted"
+      unchecked-value="not_accepted"
+    >
+      Ich akzeptiere die Sicherheitsunterweisung
+    </b-form-checkbox>
+   <!-- <p class="error" v-show="!$v.contact.$model">This field is required</p>-->
+  </div>
+  
+  <button class="btn btn-outline-secondary"  @click.prevent="back2()">zurück</button>
+  <button type="submit" class="btn btn-outline-info"  @click.prevent="submitted3()">Fertig</button>
+  
+  </b-modal>
+
+
   </div>
 </template>
 <script>
@@ -90,6 +122,7 @@ export default {
         search: '',
         query: '',
         selectedUser: null,
+        status: 'not accepted',
         
 
       }
@@ -111,23 +144,45 @@ export default {
   },
     methods: {
       submitted(userData, users){
-        var data1 = []
-        console.log("hier")
+       
+        
         this.$refs.modal3.show()
         this.$refs.modal2.hide()
-                // console.log("users", users)
+        // console.log("users", users)
         return users
       },
 
       submitted2(){
-         
+        this.$refs.modal3.hide()
+        this.$refs.modal4.show()
       },
 
+      submitted3(selectedUser, status){
+        console.log('status', this.status)
+      if(this.status != 'accepted'){
+          //console.log(status)
+          //this.$swal({
+          //  icon: 'error',
+           // text: 'Bitte die Bedingungen akzeptieren'
+          //})
+          alert('falsch')
+        }
+
+        else{
+          this.$refs.modal4.hide()
+          window.location.href = 'https://jabberguest.bechtle.com/call/'+ this.selectedUser.email
+          
+        }
+      },
       back1(){
         this.$refs.modal3.hide()
         this.$refs.modal2.show()
       },
 
+      back2(){      
+        this.$refs.modal4.hide()
+        this.$refs.modal3.show()
+      },
       
     }
 
